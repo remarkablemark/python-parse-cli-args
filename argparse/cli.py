63a14fd5148d4d2c1fb036640879ca97ba84e912
Mark <mark@remarkablemark.org>
2025-03-24 14:35:47 -0400

    refactor(argparse): tidy cli

--- argparse/cli.py
+++ argparse/cli.py
@@ -1,11 +1,18 @@
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description="A 'Hello, world!' program")
+parser = ArgumentParser(
+    prog="argparse/cli",
+    description="'Hello, world!' program",
+    epilog="https://docs.python.org/library/argparse.html"
+    # add_help=False
+)
 
-parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.2.3")
-parser.add_argument("name", help="Your name")
-parser.add_argument("-s", "--salutation", default="Hello", help="A greeting")
-parser.add_argument("-c", "--color", action="store_true", help="Colorize the text")
+parser.add_argument("--version", "-v", action="version", version="%(prog)s 1.2.3")
+# parser.add_argument("--help", action="help", help="show this help message and exit")
+
+parser.add_argument("name", help="name to greet")
+parser.add_argument("--salutation", "-s", default="Hello", help="greeting or salutation")
+parser.add_argument("--color", "-c", action="store_true", help="colorize the text")
 
 args = parser.parse_args()
 
